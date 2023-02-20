80e592561e4e2e07a2e3acc4de8a2e840d221ebd
Eric Härtel <eric.haertel@avocargo.one>
2023-02-20 21:30:45 +0100

    initial setup and some views

---
+++ lib/views/login.dart
@@ -0,0 +1,68 @@
+import 'package:erics_app/route/route.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<LoginPage> createState() => LoginState();
+}
+
+class LoginState extends State<LoginPage> {
+  TextEditingController loginController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(50),
+          child: ListView(
+            children: [
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  controller: loginController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'login',
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'password',
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: ElevatedButton(
+                  child: const Text('Login'),
+                  onPressed: () {
+                    print(loginController.text);
+                    print(passwordController.text);
+                    if( loginController.text == "eric" && passwordController.text == "password") {
+                      Navigator.pushNamed(context, home);
+                    }
+                  },
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
